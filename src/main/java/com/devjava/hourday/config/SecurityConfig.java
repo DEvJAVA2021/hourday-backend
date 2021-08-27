e9a091b0b0cffaef0c9af3540365e6e3d9985483
ayoung0073 <ayong0310@naver.com>
2021-08-27 10:03:11 +0900

    add: Cors 해결

--- src/main/java/com/devjava/hourday/config/SecurityConfig.java
+++ src/main/java/com/devjava/hourday/config/SecurityConfig.java
@@ -3,21 +3,30 @@ package com.devjava.hourday.config;
 import com.devjava.hourday.common.jwt.component.JwtAuthenticationFilter;
 import com.devjava.hourday.common.jwt.component.JwtResolver;
 import com.devjava.hourday.common.jwt.component.JwtValidator;
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final AdminServerProperties adminServer;
     private final JwtResolver jwtResolver;
     private final JwtValidator jwtValidator;
 
@@ -28,13 +37,54 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/api/users/**").permitAll()
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+//        successHandler.setTargetUrlParameter("redirectTo");
+//        successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
+        http
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/api/users/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .csrf().disable()
-                .formLogin()
+                .cors().configurationSource(corsConfigurationSource())
                 .and()
+                .csrf().disable() // 403
+
+//                .antMatchers("/api/**").hasRole("OWNER")
+//                .antMatchers(HttpMethod.PUT, "/api/**").hasAnyRole("USER", "OWNER")
+//                .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("USER", "OWNER")
+//                .antMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole("USER", "OWNER")
+//                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "OWNER")
+//                .anyRequest().permitAll()
+//                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+//                .antMatchers(this.adminServer.path("/assets/**")).permitAll() // <1>
+//                .antMatchers(this.adminServer.path("/login")).permitAll()
+
+//                .and()
+//                .cors()
+//                .and()
+//                .formLogin().loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
+//                .csrf()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .ignoringAntMatchers(
+//                        this.adminServer.path("/instances"),
+//                        this.adminServer.path("/actuator/**")
+//                )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtResolver, jwtValidator), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOrigin("http://127.0.0.1:3000");
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/devjava/hourday/config/WebConfig.java
+++ src/main/java/com/devjava/hourday/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.devjava.hourday.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,12 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.PATCH.name())
                 .allowCredentials(false);
     }
 }
